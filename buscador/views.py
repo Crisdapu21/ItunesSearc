cfe33a05fbf2d024eee728f36785348cc1e42f51
crisdapu21 <febrero21>
2016-08-18 12:18:35 -0500

    Parametrizando busqueda

--- buscador/views.py
+++ buscador/views.py
@@ -3,14 +3,36 @@ import requests
 # Create your views here.
 
 URL = "https://itunes.apple.com/search"
+tipo="all"
 
 def buscar(request):
     if request.method == "GET":
         dict=None
+
+
+        #if algo:
+            tipo="entity=software"
+
+        #elif algo2:
+
+            tipo="entity=movie"
+
+        #elif algo3:
+
+            tipo="entity=music"
+
         if 'criterio' in request.GET:
             criterio =  request.GET['criterio']
-            resource = "%s?term=%s&country=CO&limit=5" % ( URL, criterio)
+
+            #si es software
+
+            resource = "%s?term=%s&country=CO&%s&limit=20" %(URL,criterio,tipo)
+
+
             response = requests.get(  resource )
             data = response.json()
+
             dict={'data':data["results"],'criterio':criterio}
     return render(request, "index.html",dict)
+
+
