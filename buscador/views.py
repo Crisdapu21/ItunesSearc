adac46db5d49d5d8ca952f69bb23fedaaf022da7
crisdapu21 <febrero21>
2016-08-19 20:03:46 -0500

    Parametrizando Filtros

--- buscador/views.py
+++ buscador/views.py
@@ -3,36 +3,34 @@ import requests
 # Create your views here.
 
 URL = "https://itunes.apple.com/search"
-tipo="all"
+
 
 def buscar(request):
     if request.method == "GET":
         dict=None
 
 
-        #if algo:
-            tipo="entity=software"
-
-        #elif algo2:
-
-            tipo="entity=movie"
-
-        #elif algo3:
-
-            tipo="entity=music"
-
         if 'criterio' in request.GET:
             criterio =  request.GET['criterio']
-
-            #si es software
-
-            resource = "%s?term=%s&country=CO&%s&limit=20" %(URL,criterio,tipo)
+            filtro1 = request.GET['Filtro1']
 
 
-            response = requests.get(  resource )
+            tipo=(filtro1)
+
+            variable="entity="
+
+            resource = "%s?term=%s&country=CO&%s%s&limit=20" % (URL, criterio,variable,tipo)
+
+            response = requests.get(resource)
             data = response.json()
 
-            dict={'data':data["results"],'criterio':criterio}
-    return render(request, "index.html",dict)
 
+
+            dict = {'data': data["results"], 'criterio': criterio}
+
+
+
+            #tipo="entity=software"
+
+    return render(request, "index.html",dict)
 
